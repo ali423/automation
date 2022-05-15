5a8ef2684b4de30daebd3871f4162bfc35b8a0fa
maalireza51 <maalireza51@yahoo.com>
2022-05-16 01:10:28 +0430

    datatable export

---
+++ public/js/customDataTable.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    pdfMake.fonts = {
+        Roboto: {
+            normal: 'Roboto-Regular.ttf',
+            bold: 'Roboto-Medium.ttf',
+            italics: 'Roboto-Italic.ttf',
+            bolditalics: 'Roboto-MediumItalic.ttf'
+        },
+        IRANSansWeb: {
+            normal: "IRANSansWeb400.ttf",
+            bold: "IRANSansWeb400.ttf",
+            italics: "IRANSansWeb400.ttf",
+            bolditalics: "IRANSansWeb400.ttf"
+        }
+    };
+
+    $('#datatable-buttons').DataTable({
+        dom: 'Bfrtip',
+        buttons: [{
+                extend: 'copy',
+                text: "کپی",
+                className: 'btn btn-outline-primary'
+            },
+            {
+                extend: 'pdf',
+                text: 'pdf',
+                className: 'btn btn-outline-primary',
+                exportOptions: {
+                    columns: [4, 3, 2, 1, 0],
+                    modifier: {
+                        page: 'current'
+                    },
+                    orthogonal: "rtlexport"
+                },
+                customize: function(doc) {
+                    doc.defaultStyle.font = "IRANSansWeb";
+                    doc.content[1].table.widths = ['20%', '20%', '20%', '20%', '20%'];
+                    doc.styles.tableBodyEven.alignment = 'center';
+                    doc.styles.tableBodyOdd.alignment = 'center';
+                }
+            },
+            {
+                extend: 'excel',
+                className: 'btn btn-outline-primary',
+                exportOptions: {
+                    columns: [4, 3, 2, 1, 0],
+                    modifier: {
+                        page: 'current'
+                    }
+                }
+            },
+            {
+                extend: 'csv',
+                className: 'btn btn-outline-primary'
+            },
+            {
+                extend: 'print',
+                text: "پرینت",
+                className: 'btn btn-outline-primary'
+            }
+        ],
+        columnDefs: [{
+            targets: '_all',
+            render: function(data, type, row) {
+                if (type === 'rtlexport') {
+                    return data.split(' ').reverse().join(' ');
+                }
+                return data;
+            }
+        }]
+    });
+});
--- resources/views/dashboard/role/index.blade.php
+++ resources/views/dashboard/role/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title','داشبورد')
+@section('title', 'داشبورد')
 
 @section('page_styles')
     <!-- These plugins only need for the run this page -->
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/default-assets/responsive.bootstrap4.css') }}">
     <link rel="stylesheet" href="{{ asset('css/default-assets/buttons.bootstrap4.css') }}">
     <link rel="stylesheet" href="{{ asset('css/default-assets/select.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/datatables-td.css') }}">
 @endsection
 
 @section('content')
@@ -17,50 +18,53 @@
                     <h4 class="card-title mb-2">لیست نقش ها</h4>
                     <table id="datatable-buttons" class="table table-striped dt-responsive nowrap w-100">
                         <thead class="text-center">
-                        <tr>
-                            <th>ردیف</th>
-                            <th>نام</th>
-                            <th>عنوان</th>
-                            <th>تاریخ ایجاد</th>
-                            <th>ایجاد کننده</th>
-                            <th>جزئیات</th>
-                        </tr>
+                            <tr>
+                                <th>ردیف</th>
+                                <th>نام</th>
+                                <th>عنوان</th>
+                                <th>تاریخ ایجاد</th>
+                                <th>ایجاد کننده</th>
+                                <th>جزئیات</th>
+                            </tr>
                         </thead>
 
                         <tbody class="text-center">
-                        @php($i=1)
-                        @foreach($roles as $role)
-                        <tr>
-                            <td>{{ $i }}</td>
-                            <td>{{ $role->name }}</td>
-                            <td>{{ $role->title }}</td>
-                            <td>{{\Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strtotime($role->created_at))}}</td>
-                            <td>ادمین</td>
-                            <td><a href="{{route('role.show',$role)}}"><i class="fa fa-object-group"></i></a></td>
-                        </tr>
-                            @php($i++)
-                        @endforeach
+                            @php($i = 1)
+                            @foreach ($roles as $role)
+                                <tr>
+                                    <td>{{ $i }}</td>
+                                    <td>{{ $role->name }}</td>
+                                    <td>{{ $role->title }}</td>
+                                    <td>{{ \Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strtotime($role->created_at)) }}
+                                    </td>
+                                    <td>ادمین</td>
+                                    <td><a href="{{ route('role.show', $role) }}"><i class="fa fa-object-group"></i></a>
+                                    </td>
+                                </tr>
+                                @php($i++)
+                            @endforeach
                         </tbody>
                     </table>
-
                 </div> <!-- end card body-->
             </div> <!-- end card -->
         </div><!-- end col-->
     </div>
-    @endsection
+@endsection
 
-    @section('page_scripts')
-        <!-- These plugins only need for the run this page -->
-        <script src="{{asset('js/default-assets/jquery.datatables.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/datatables.bootstrap4.js')}}"></script>
-        <script src="{{asset('js/default-assets/datatable-responsive.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/responsive.bootstrap4.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/datatable-button.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/button.bootstrap4.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/button.html5.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/button.flash.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/button.print.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/datatables.keytable.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/datatables.select.min.js')}}"></script>
-        <script src="{{asset('js/default-assets/demo.datatable-init.js')}}"></script>
+
+@section('page_scripts')
+
+
+    <script src="{{ asset('js/default-assets/jquery.datatables.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/datatable-responsive.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/jszip.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('js/default-assets/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/button.print.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/dataTables.sorting.persian.js') }}"></script>
+    <script src="{{ asset('js/default-assets/customDataTable.js') }}"></script>
+
 @endsection
