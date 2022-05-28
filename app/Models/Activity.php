215d8b1926ba18c5a98d824a96c476c4cdba62d7
ali423 <48999122+ali423@users.noreply.github.com>
2022-05-28 15:53:02 +0430

    fix activity relation name

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -19,6 +19,7 @@ class Activity extends Model
         'previous_activity_id',
         'pivot_data',
         'relation_model',
+        'relation_name',
     ];
 
     public function user(){
@@ -51,8 +52,8 @@ class Activity extends Model
             case "update":
                 $old_value=json_decode($this->perviousActivity->data,true);
                 $new_value=json_decode($this->data,true);
-                unset($old_value['permissions'],$old_value['updated_at']);
-                unset($new_value['permissions'],$new_value['updated_at']);
+                unset($old_value[$this->relation_name],$old_value['updated_at']);
+                unset($new_value[$this->relation_name],$new_value['updated_at']);
                 $old_diff=array_diff($old_value,$new_value);
                 $new_diff=array_diff($new_value,$old_value);
                return [
