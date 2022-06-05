d9a0edfc9b548e3702e13aedb221e6e108c141f2
ali423 <48999122+ali423@users.noreply.github.com>
2022-06-05 13:39:16 +0430

    add warehouse crud to project

---
+++ app/Http/Controllers/WarehouseController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\WarehouseRequest;
+use App\Http\Requests\WarehouseUpdateRequest;
+use App\Models\Warehouse;
+use App\Services\WarehouseService;
+use Illuminate\Http\Request;
+
+class WarehouseController extends Controller
+{
+
+    protected $service;
+
+    public function __construct(WarehouseService $service)
+    {
+        $this->service=$service;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $warehouses=Warehouse::query()->orderBy('id', 'DESC')->get();
+        return view('dashboard.warehouse.index',
+            [
+                'warehouses'=>$warehouses,
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard.warehouse.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function store(WarehouseRequest $request)
+    {
+        $this->service->create($request->only('title','type','capacity','status'));
+        return redirect(route('warehouse.index'))->with('successful', 'اطلاعات ثبت شد.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function show(Warehouse $warehouse)
+    {
+        return view('dashboard.warehouse.show',[
+            'warehouse'=>$warehouse,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit(Warehouse $warehouse)
+    {
+        return view('dashboard.warehouse.edit',[
+            'warehouse'=>$warehouse,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function update(WarehouseUpdateRequest $request, Warehouse $warehouse)
+    {
+        $this->service->update($warehouse,$request->only('title','type','capacity','status'));
+        return redirect(route('warehouse.show',$warehouse))->with('successful', 'اطلاعات ویرایش شدند.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Warehouse  $warehouse
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function destroy(Warehouse $warehouse)
+    {
+        $warehouse->delete();
+        return redirect(route('warehouse.index'))->with('successful', 'اطلاعات حذف شدند.');
+    }
+}
---
+++ resources/views/dashboard/warehouse/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.main')
+@section('title', 'داشبورد')
+@section('page_styles')
+    <!-- These plugins only need for the run this page -->
+    <link rel="stylesheet" href="{{ asset('css/default-assets/datatables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default-assets/responsive.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default-assets/buttons.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/default-assets/select.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/datatables-td.css') }}">
+
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-12 box-margin">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title mb-2">لیست انبار ها</h4>
+                    <table id="datatable-buttons" class="table table-striped dt-responsive nowrap w-100">
+                        <thead class="text-center">
+                            <tr>
+                                <th>ردیف</th>
+                                <th> {{ __('fields.title') }}</th>
+                                <th> {{ __('fields.type') }}</th>
+                                <th> {{ __('fields.status') }}</th>
+                                <th> {{ __('fields.capacity') }}</th>
+                                <th>{{ __('fields.created_at') }}</th>
+                                <th>{{ __('fields.creator') }}</th>
+                                <th>{{ __('fields.details') }}</th>
+                            </tr>
+                        </thead>
+
+                        <tbody class="text-center">
+                            @php($i = 1)
+                            @foreach ($warehouses as $warehouse)
+                                <tr>
+                                    <td>{{ $i }}</td>
+                                    <td>{{ $warehouse->title }}</td>
+                                    <td>{{ __('fields.warehouse.types') [$warehouse->type] }}</td>
+                                    <td>{{__('fields.warehouse.status')[$warehouse->status]  }}</td>
+                                    <td>{{ $warehouse->capacity }}</td>
+                                    <td>{{ \Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strtotime($warehouse->created_at)) }}
+                                    </td>
+                                    @if(isset($warehouse->creator_user))
+                                    <td>{{ $warehouse->creator_user->full_name }}</td>
+                                    @else
+                                        <td>سیستم</td>
+                                    @endif
+                                    <td><a href="{{ route('warehouse.show', $warehouse) }}" class=""><i class="ti-more-alt font-24"></i></a>
+                                    </td>
+                                </tr>
+                                @php($i++)
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                </div> <!-- end card body-->
+            </div> <!-- end card -->
+        </div><!-- end col-->
+    </div>
+@endsection
+
+@section('page_scripts')
+
+
+    <script src="{{ asset('js/default-assets/jquery.datatables.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/datatable-responsive.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/jszip.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('js/default-assets/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/button.print.min.js') }}"></script>
+    <script src="{{ asset('js/default-assets/dataTables.sorting.persian.js') }}"></script>
+    <script src="{{ asset('js/default-assets/customDataTable.js') }}"></script>
+
+@endsection
