87fff646cb16298e3e2734eb4b9b6b5be60815f2
ali423 <48999122+ali423@users.noreply.github.com>
2022-06-19 13:18:52 +0430

    add file morph relation

---
+++ app/Models/File.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class File extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'commentable_id',
+        'commentable_type',
+        'body',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function recordChange()
+    {
+        return $this->morphTo()->withTrashed();
+    }
+}
---
+++ database/migrations/2022_06_18_131135_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('owner_id');
+            $table->string('owner_type');
+            $table->string('source');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
