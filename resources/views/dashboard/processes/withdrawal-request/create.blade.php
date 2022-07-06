2f372e1c075850507317b44aa2c93cdfe19b2ab5
ali423 <48999122+ali423@users.noreply.github.com>
2022-07-06 16:48:49 +0430

    add ajax for getting inventory

--- resources/views/dashboard/processes/withdrawal-request/create.blade.php
+++ resources/views/dashboard/processes/withdrawal-request/create.blade.php
@@ -13,7 +13,8 @@
 
                 <div class="row">
                     <div class="col-sm-12 col-xs-12">
-                        <form method="post" action="{{ route('withdrawal-request.store') }}" class="needs-validation forms-sample" enctype="multipart/form-data" novalidate="">
+                        <form method="post" action="{{ route('withdrawal-request.store') }}"
+                              class="needs-validation forms-sample" enctype="multipart/form-data" novalidate="">
                             @csrf
                             <div id="product_formul" class="col-lg-12">
                                 <p>اطلاعات ورود کالا به انبار</p>
@@ -27,7 +28,9 @@
                                                 </option>
                                             @endforeach
                                         </select>
-                                        <div class="invalid-feedback">{{ __('fields.commodity.name') }} را انتخاب کنید.</div>
+                                        <div class="invalid-feedback">{{ __('fields.commodity.name') }} را انتخاب
+                                            کنید.
+                                        </div>
                                     </div>
                                     <div class="form-group col-md-4">
                                         <label for="unit"> {{ __('fields.unit') }}</label>
@@ -42,7 +45,8 @@
                                     </div>
                                     <div class="form-group col-md-4">
                                         <label for="sell-price"> {{ __('fields.sell-price') }}</label>
-                                        <input type="text" id="sell-price" name="" class="form-control" placeholder="{{ __('fields.sell-price') }}" required>
+                                        <input type="text" id="sell-price" name="" class="form-control"
+                                               placeholder="{{ __('fields.sell-price') }}" required>
                                         <div class="invalid-feedback">{{ __('fields.unit') }} را انتخاب کنید</div>
                                     </div>
                                 </div>
@@ -53,18 +57,22 @@
 
                             <div class="form-group">
                                 <label>الصاق فایل به درخواست</label>
-                                <input type="file" name="file" class="file-upload-default" accept="image/*,.pdf,.zip,.rar">
+                                <input type="file" name="file" class="file-upload-default"
+                                       accept="image/*,.pdf,.zip,.rar">
                                 <div class="input-group col-xs-12">
-                                    <input type="text" class="form-control file-upload-info" disabled="" placeholder="فایل از نوع تصویر ، pdf یا zip">
+                                    <input type="text" class="form-control file-upload-info" disabled=""
+                                           placeholder="فایل از نوع تصویر ، pdf یا zip">
                                     <span class="input-group-append">
-                                        <button class="file-upload-browse btn btn-primary" type="button">انتخاب فایل</button>
+                                        <button class="file-upload-browse btn btn-primary"
+                                                type="button">انتخاب فایل</button>
                                     </span>
                                 </div>
                             </div>
 
                             <div class="form-group mb-20">
                                 <label for="comment">توضیحات</label>
-                                <textarea class="form-control rounded-0 form-control-md" name="comment" id="comment" rows="6">{{old('comment')}}</textarea>
+                                <textarea class="form-control rounded-0 form-control-md" name="comment" id="comment"
+                                          rows="6">{{old('comment')}}</textarea>
                             </div>
 
                             <button type="submit" class="btn btn-primary mr-2">ثبت درخواست</button>
@@ -82,28 +90,54 @@
 
     <script type="text/javascript">
         // add row
-        $("#addRow").click(function() {
+        $("#addRow").click(function () {
             var html = '<div id="inputFormRow" class="form-row shadow p-4 mb-3"> <div class="form-group col-md-6"> <label for="commodity_id"> {{ __('fields.commodity.name') }}</label> <select id="commodity_id" class="form-control" name="commodity_id[]" required> <option value="">انتخاب کنید</option>@foreach ($commodities as $commodity)<option value="{{ $commodity->id }}">{{ $commodity->title }}</option>@endforeach</select> <div class="invalid-feedback">{{ __('fields.commodity.name') }} را انتخاب کنید.</div> </div> <div class="form-group col-md-6"> <label for="unit"> {{ __('fields.unit') }}</label> <select id="unit" class="form-control" name="unit[]" required> <option value="">انتخاب کنید...</option>@foreach( __('fields.commodity.units') as $key=>$value)<option value="{{$key}}">{{$value}}</option>@endforeach</select> <div class="invalid-feedback">{{ __('fields.unit') }} را انتخاب کنید</div> </div> <div class="form-group col-md-6"> <label for="warehouse_id"> {{ __('fields.warehouse.name') }}</label> <select id="warehouse_id" class="form-control" name="warehouse_id[]" required> <option value="">انتخاب کنید</option>@foreach ($warehouses as $warehouse)<option value="{{ $warehouse->id }}">{{ $warehouse->title }}</option>@endforeach</select> <div class="invalid-feedback">{{ __('fields.warehouse.name') }} را انتخاب کنید.</div> </div> <div class="form-group col-md-6"> <label for="amount"> {{  __('fields.commodity.amount') }}</label> <input type="number" min="1" name="amount[]" class="form-control"id="amount" autocomplete="off" placeholder="{{  __('fields.commodity.amount') }}" pattern="[0-9 .]"  required=""> <div class="invalid-feedback">لطفاً {{  __('fields.commodity.amount') }} را وارد کنید. </div></div> <i id="removeRow" type="submit" class="ti-close"></i></div></div>';
             $('#newRow').append(html);
-            document.querySelectorAll('#inputFormRow').forEach((element,index) => {
-                element.querySelector('#commodity_id').setAttribute('name', 'commodity_id['+index+']');
-                element.querySelector('#unit').setAttribute('name', 'unit['+index+']');
-                element.querySelector('#warehouse_id').setAttribute('name', 'warehouse_id['+index+']');
-                element.querySelector('#amount').setAttribute('name', 'amount['+index+']');
+            document.querySelectorAll('#inputFormRow').forEach((element, index) => {
+                element.querySelector('#commodity_id').setAttribute('name', 'commodity_id[' + index + ']');
+                element.querySelector('#unit').setAttribute('name', 'unit[' + index + ']');
+                element.querySelector('#warehouse_id').setAttribute('name', 'warehouse_id[' + index + ']');
+                element.querySelector('#amount').setAttribute('name', 'amount[' + index + ']');
             });
         });
 
         // remove row
-        $(document).on('click', '#removeRow', function() {
+        $(document).on('click', '#removeRow', function () {
             $(this).closest('#inputFormRow').remove();
-            document.querySelectorAll('#inputFormRow').forEach((element,index) => {
-                element.querySelector('#commodity_id').setAttribute('name', 'commodity_id['+index+']');
-                element.querySelector('#unit').setAttribute('name', 'unit['+index+']');
-                element.querySelector('#warehouse_id').setAttribute('name', 'warehouse_id['+index+']');
-                element.querySelector('#amount').setAttribute('name', 'amount['+index+']');
+            document.querySelectorAll('#inputFormRow').forEach((element, index) => {
+                element.querySelector('#commodity_id').setAttribute('name', 'commodity_id[' + index + ']');
+                element.querySelector('#unit').setAttribute('name', 'unit[' + index + ']');
+                element.querySelector('#warehouse_id').setAttribute('name', 'warehouse_id[' + index + ']');
+                element.querySelector('#amount').setAttribute('name', 'amount[' + index + ']');
             });
         });
     </script>
+
+    <script type='text/javascript'>
+        $(document).ready(function () {
+
+            $('#commodity_id').change(function () {
+
+                // Department id
+                var id = $(this).val();
+
+                // AJAX request
+                $.ajax({
+                    url: '/inventory-ajax/' + id,
+                    type: 'get',
+                    dataType: 'json',
+                    success: function (response) {
+                        var price = response['price'];
+                        var warehouses = response['warehouses'];
+                        // alert(warehouses['0']['title']);
+                        // alert(warehouses['0']['id']);
+                        // alert(warehouses['0']['amount']);
+                    }
+                });
+            });
+        });
+    </script>
+
     <!-- These plugins only need for the run this page -->
 
     <script src="{{ asset('js/default-assets/active.js') }}"></script>
