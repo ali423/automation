4693c5d63273a10e5761df6a3b97c29988d8bcf8
maalireza51 <maalireza51@yahoo.com>
2022-07-12 00:00:49 +0430

    invoice-finvoice-table & totalamount

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
             ];
         }
         return view('dashboard.index',[
-            'commodities'=>$res,
+            'commodities'=>$res?? null,
         ]);
     }
 }
