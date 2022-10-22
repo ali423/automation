d15823e554a27b72214c7231eea5d6b0fd269458
Ali Mokhtari <ali.mokhtari@snapp.market>
2022-10-22 11:01:20 +0330

    test22222

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\View;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-//test
+//test2
 
     public function shareView()
     {
