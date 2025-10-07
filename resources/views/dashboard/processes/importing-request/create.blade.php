a826812e8db5e5802f1e197b122fa79f4fba2204
ryderys <youssefi.ashkan.ys@gmail.com>
2025-10-07 19:37:14 +0330

    modified seller dropdowns to use company name

--- resources/views/dashboard/processes/importing-request/create.blade.php
+++ resources/views/dashboard/processes/importing-request/create.blade.php
@@ -17,11 +17,11 @@
                             @csrf
                             <div class="form-row m-3">
                                 <div class="form-group col">
-                                    <label for="seller_id">نام فروشنده</label>
+                                    <label for="seller_id">نام شرکت</label>
                                     <select id="seller_id" class="form-control" name="seller_id" required>
                                         <option value="">انتخاب کنید</option>
                                         @foreach ($sellers as $seller)
-                                            <option value="{{ $seller->id }}">{{$seller->name}}</option>
+                                            <option value="{{ $seller->id }}">{{$seller->comp_name ?? $seller->name}}</option>
                                         @endforeach
                                     </select>
                                     <div class="invalid-feedback">
